1125fa84959f1f89c2150adac649e496246c6f29
JunHyun <grssa@naver.com>
2021-03-10 17:18:12 +0900

    Add files via upload
    11053

---
+++ baekjoon_11053/src/baekjoon_11053/Main.java
@@ -0,0 +1,36 @@
+package baekjoon_11053;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {
+					if (dp[j] >= dp[i]) {
+						dp[i] = dp[j] + 1;
+					}
+				}
+			}
+			
+		}
+		Arrays.sort(dp);
+		
+		System.out.println(dp[n-1]);
+		for (int i = 0; i < dp.length; i++) {
+			System.out.println(dp[i]);
+		}
+	}
+}
